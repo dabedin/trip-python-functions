80b9f81c8eb16b7ea85567c36692f8a09f347d64
Davide Bedin <davide.bedin@outlook.com>
2023-03-08 17:22:47 +0100

    cleanup

--- zip-deploy.ps1
+++ zip-deploy.ps1
@@ -1,11 +1,14 @@
 $funcAppName = "REPLACE"
 $resGroupName = "REPLACE"
 $funcSlotName = "REPLACE"
+$PATH = "REPLACE"
+
+$hostName = (az functionapp show --name $funcAppName --resource-group $resGroupName --query "hostNames[0]" --output tsv)
 
 #remember python zip structure https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-python?tabs=asgi%2Capplication-level#folder-structure
 
 # Zip deployment https://docs.microsoft.com/en-us/azure/azure-functions/deployment-zip-push
-az functionapp deployment source config-zip -g $resGroupName -n $funcAppName --src private\deploy.zip
+az functionapp deployment source config-zip -g $resGroupName -n $funcAppName --src $PATH\deploy.zip
 # alternative via API https://docs.microsoft.com/en-us/azure/azure-functions/deployment-zip-push#rest
 
 # Run from package https://docs.microsoft.com/en-us/azure/azure-functions/run-functions-from-deployment-package#adding-the-website_run_from_package-setting
@@ -13,7 +16,10 @@ az webapp config appsettings set -n $webAppName -g $resGroupName `
     --settings "WEBSITE_RUN_FROM_PACKAGE=1"
 
 # deploy to deployment slot
-az functionapp deployment source config-zip -g $resGroupName -n $funcAppName --slot $funcSlotName --src private\deploy.zip
+az functionapp deployment source config-zip -g $resGroupName -n $funcAppName --slot $funcSlotName --src $PATH\deploy.zip
 
 # slot swap https://docs.microsoft.com/en-us/cli/azure/functionapp/deployment/slot?view=azure-cli-latest#az-functionapp-deployment-slot-swap
 az functionapp deployment slot swap -g $resGroupName -n $funcAppName --slot $funcSlotName --target-slot production
+
+# test
+Invoke-RestMethod ($hostName + '/api/Test?name=tester')
